63f19b4df5d72a569b626299b788952fe402db9d
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2017-07-24 00:48:38 +0200

    New extended version
    1. Specify clock as a parameter, thus one site may handle several clocks.
    2. Clock hands identified with class, no more with id.
    3. May handle month, day, hour, minute and second hands

--- Dial-clock/MilesianClockOperations.js
+++ Dial-clock/MilesianClockOperations.js
@@ -23,13 +23,24 @@
 // or the use or other dealings in the software.
 // Inquiries: www.calendriermilesien.org
 */
-function setSolarYearClockHands(month = 0, day = 1) { 
-//	Set the angles of the "month hand" and of the "day hand" of the milesian solar clock, from the date and month given
-	let halfDays = 60*month + 2*Math.floor(month/2) + 2*day;	
+function setSolarYearClockHands(clock, month = 0, day = 1, hour = 24, minutes = 0, seconds = 0) { 
+//	On "clock" object, set all available hands (month, day, hour, minutes, seconds)
+//  If no time in the day given, set hands at end of day.
+//  If no date given, set to 1 1m.
+	let halfDays = 60*month + 2*Math.floor(month/2) + 2*(day-1) + (hour + minutes/60 + seconds/3600)/12;	
 	// Number of half-days since beginning of year, at beginning of day i.e. at THE END of that day
 	let	monthAngle = halfDays * 360 / 732, 			// Angle of month hand with respect to vertical upright
-		dayAngle = 	 halfDays * 360 * 12 / 732;		// Angle of day hand with respect to vertical upright
-	document.querySelector("#monthhand").transform.baseVal[0].setRotate(monthAngle,0,0);	// Use SVG interfaces to set angles
-	document.querySelector("#dayhand").transform.baseVal[0].setRotate(dayAngle,0,0);
+		dayAngle = 	 halfDays * 360 * 12 / 732,		// Angle of day hand with respect to vertical upright
+		hourAngle = hour*30, minuteAngle = minutes*6, secondAngle = seconds*6;
+	let theHand = clock.querySelector(".monthhand");
+	if (theHand !== null) theHand.transform.baseVal[0].setRotate(monthAngle,0,0);	// Use SVG interfaces to set angles
+	theHand = clock.querySelector(".dayhand");
+	if (theHand !== null) theHand.transform.baseVal[0].setRotate(dayAngle,0,0);
+	theHand = clock.querySelector(".hourhand");
+	if (theHand !== null) theHand.transform.baseVal[0].setRotate(hourAngle,0,0);
+	theHand = clock.querySelector(".minutehand");
+	if (theHand !== null) theHand.transform.baseVal[0].setRotate(minteAngle,0,0);
+	theHand = clock.querySelector(".hourhand");
+	if (theHand !== null) theHand.transform.baseVal[0].setRotate(secondAngle,0,0);
 	return halfDays;	// control the computation parameters with the return value
 }
