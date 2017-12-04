0c976d1a554bee1fc5b9452921271944f295e74e
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2017-12-04 15:27:03 +0100

    Version tested as standalone
    Tested for the French Revolutionary calendar. Useless lines deleted.

--- Misc_Date_Properties/CalendarShiftCycleComputationEngine.js
+++ Misc_Date_Properties/CalendarShiftCycleComputationEngine.js
@@ -5,15 +5,13 @@
 // This package perform similar functions to CalendarCycleComputationEngine 
 // except that it is adapted to calendar which "shift" the intercalation year by one on a regular basis.
 // This version is particularly usefull for the extended French Revolutionary calendar
-// for which a longer ("sextile") year occur ordinarly every 4 year, but sometimes after 5 years.
-// The routine  then compute.
+// for which a longer ("sextile") year occur ordinarly every 4 year, but sometimes after 5 years, building 33 years cycles.
 // the rank of a day within a year, and then hours, minutes, seconds and milliseconds.
-// Computations on months require more specific algorithms.
 //
-// Version 1 : M2017-12-13
-// Version 1: 
-//	Changed the names of the functions
-//	The parameters are now in each package. Only very common parameters and variables are defined here.
+// Note: This package must be used together with Calendar Cycle Computation Engine.
+//
+// Version 1 : M2017-12-14
+// Version 1: tested for the French Revolutionary calendar
 //
 *//////////////////////////////////////////////////////////////////////////////////////////////
 /* Copyright Miletus 2017 - Louis A. de Fouquières
@@ -37,49 +35,14 @@
 // or the use or other dealings in the software.
 // Inquiries: www.calendriermilesien.org
 *////////////////////////////////////////////////////////////////////////////////
-/* var Chronos = { // Set of chronological constants generally used when handling Unix dates.
-  DAY_UNIT : 86400000, // One day in Unix time units
-  HOUR_UNIT : 3600000,
-  MINUTE_UNIT : 60000,
-  SECOND_UNIT : 1000
-}
-*/
-/* Parameter object structure. Replace # with numbers or literals.
-var decomposeParameterExample = {
-	timeepoch : #, origin time in milliseconds (or in the suitable unit) to be used for the decomposition, with respect to 1/1/1970 00:00 UTC.
-	coeff : [ // This array holds the coefficient used to decompose a time stamp into time cycles like eras, quadrisaeculae, centuries etc.
-		{cyclelength : #, //length of the cycle, expressed in milliseconds.
-		ceiling : #, // Infinity, or the maximum number of cycles of this size minus one in the upper cycle; the last cycle may hold an intercalary unit.
-		multiplier : #, // multiplies the number of cycle of this level to convert into target units.
-		target : #, // the unit (e.g. "year") of the decomposition element at this level. 
-		} ,
-		{ // similar elements at the lower cycle level 
-		} // end of array element
-	], // End of this array, but not end of object
-	canvas : [ // this last array is the canvas of the decomposition , e.g. "year", "month", "date", with suitable properties at each level.
-		{ name : #, // the name of the property at this level, which must match one target property of the coeff component,
-		init : #, // value of this component at epoch, and lowest value (except for the first component), e.g. 0 for month, 1 for date, 0 for hours, minutes, seconds.
-		} // End of array element (only two properties)
-	] // End of second array
-}	// End of object.
+//
+// Parameter object structure: same as in Calendar Cycle Computation Engine. 
+//
 // Constraints: 
 //	1. 	The cycles and the canvas elements shall be definined from the larger to the smaller 
-//		e.g. quadrisaeculum, then century, then quadriannum, then year, etc.
-//	2. 	The same names shall bu used for the "coeff" and the "canvas" properties, otherwise applications may return "NaN".
+//		e.g. (for French Revolutionary calendar): 33 years cycle, then Franciade (4 to 5 years), then years, the month etc.
+//	2. 	The same names shall be used for the "coeff" and the "canvas" properties, otherwise applications may return "NaN".
 //		
-*/
-/* var
-Day_milliseconds = { 	// To convert a time or a duration to and from days + milliseconds in day.
-	timeepoch : 0, 
-	coeff : [ // to be used with a Unix timestamp in ms. Decompose into days and milliseconds in day.
-	  {cyclelength : 86400000, ceiling : Infinity, multiplier : 1, target : "day_number"}, 
-	  {cyclelength : 1, ceiling : Infinity, multiplier : 1, target : "milliseconds_in_day"}
-	],
-	canvas : [
-		{name : "day_number", init : 0},
-		{name : "milliseconds_in_day", init : 0},
-	]
-}*/
 /////////////////////////////////////////////
 // csceDecompose : from time serie figure to decomposition
 ///////////////////////////////////////////// 
@@ -112,25 +75,28 @@ function csceDecompose (quantity, params) { // from a chronological number, buil
 ////////////////////////////////////////////
 // csceCompose: from compound object to time serie figure.
 ////////////////////////////////////////////
-function csceCompose (cells, params) { // from an object structured as params.canvas, compute the chronological number
-	var quantity = params.timeepoch ; // initialise quantity
+function csceCompose (cells, params) { // from an object cells structured as params.canvas, compute the chronological number
+	var quantity = params.timeepoch ; // initialise Unix quantity to computation epoch
 	for (let i = 0; i < params.canvas.length; i++) { // cells value shifted as to have all 0 if at epoch
 		cells[params.canvas[i].name] -= params.canvas[i].init
 	}
-/*	Strategy: decompose year element following cycle structure
-*/
-	let currentTarget = params.coeff[0].target; 	// Set to uppermost unit (year, most often)
-	let currentCounter = cells[params.coeff[0].target]
-	let addCycle = 0; 
+//	Unlike ccceCompose, decomposition is top-down. 
+	let currentTarget = params.coeff[0].target; 	// Set to uppermost unit used for date (year, most often)
+	let currentCounter = cells[params.coeff[0].target];	// This counter shall hold the successive remainders
+	let addCycle = 0; 	// This flag says whether there is an additionnal period at end of cycle, e.g. a 5th year in the Franciade
 	for (let i = 0; i < params.coeff.length; i++) {
-	let f = 0;				// Number of "target" values (number of years, to begin with), and remainder for each cycle
-	
-	let ceiling = params.coeff[i].ceiling + addCycle;
+		let f = 0;				// Number of "target" values (number of years, to begin with)
+		if (currentTarget != params.coeff[i].target) {	// If we go to the next level (e.g. year to month), reset variables
+			// if (currentCounter != 0) alert ("Error csceCompose, i, currentCounter: ", i, currentCounter); // Debug only
+			currentTarget = params.coeff[i].target;
+			currentCounter = cells[currentTarget];
+		}
+		let ceiling = params.coeff[i].ceiling + addCycle;
 		while (currentCounter < 0) {
 			--f;
-			currentCounter += params.coeff[i].multiplier
+			currentCounter += params.coeff[i].multiplier;
 		}
-		while (currentCounter >= params.coeff[i].multiplier) && (f < ceiling) {
+		while ((currentCounter >= params.coeff[i].multiplier) && (f < ceiling)) {
 			++f;
 			currentCounter -= params.coeff[i].multiplier;
 		}
