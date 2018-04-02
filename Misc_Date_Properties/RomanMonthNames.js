d66178d796f2cb6ebf8e109246786545af0dc87a
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2018-04-02 09:19:05 +0200

    List of Roman months FR
    First simple list of Roman months, in French only. Ideally, it should come from Unicode.

---
+++ Misc_Date_Properties/RomanMonthNames.js
@@ -0,0 +1,10 @@
+/* Roman months names
+These names are provided only because the Julian calendar
+is not yet implemented with Unicode
+Copyright Louis A. de FOUQUIERES
+Copy is possible without restriction
+*/
+var
+	romanMonthNames = {
+		fr : ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
+	}
