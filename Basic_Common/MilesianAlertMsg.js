77635787e66d1803fc140aaa1f746f16e1778f89
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2018-05-12 13:59:02 +0200

    Change comments
    Change header comments presentation, using */ rather than // lines.

--- Basic_Common/MilesianAlertMsg.js
+++ Basic_Common/MilesianAlertMsg.js
@@ -1,33 +1,41 @@
-// MilesianAlertMsg
-// Language dependant messages to be used with Milesian demonstrator routines
-// This version UTF-8 to be recoded as necessary
-// M2017-12-23
-//  Changes from M2017-08:
-//		Delete MilesianAlertMsg object (not used anymore)
-//		Suppress ":" at end of messages, but leave " " (blank character)
-//  Changes M2018-05
-//		Enhance messages in German
-//		New entry: invalid code
-/* Copyright Miletus 2016-2017 - Louis A. de Fouquières
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// 1. The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-// 2. Changes with respect to any former version shall be documented.
-//
-// The software is provided "as is", without warranty of any kind,
-// express of implied, including but not limited to the warranties of
-// merchantability, fitness for a particular purpose and noninfringement.
-// In no event shall the authors of copyright holders be liable for any
-// claim, damages or other liability, whether in an action of contract,
-// tort or otherwise, arising from, out of or in connection with the software
-// or the use or other dealings in the software.
-// Inquiries: www.calendriermilesien.org
+/* MilesianAlertMsg
+Language dependant messages to be used with Milesian demonstrator routines
+This version UTF-8 to be recoded as necessary
+Contents
+	function milesianAlertMsg (inputError) : return a string with an error message in current system language
+Versions
+	M2017-12-23, changes from M2017-08:
+		Delete MilesianAlertMsg object (no longer used)
+		Suppress ":" at end of messages, but leave " " (blank character)
+	M2018-05
+	Enhance messages in German
+	New message entries: 
+		invalidCode (for e formatting option)
+		browserError (after error catched because browser does not handle certain JS objects, like Unicode formatter objects)
+Required:
+	No other file required
+*/
+/* Copyright Miletus 2017-2018 - Louis A. de Fouquières
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sub-license, or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+1. The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+2. Changes with respect to any former version shall be documented.
+
+The software is provided "as is", without warranty of any kind,
+express of implied, including but not limited to the warranties of
+merchantability, fitness for a particular purpose and non-infringement.
+In no event shall the authors of copyright holders be liable for any
+claim, damages or other liability, whether in an action of contract,
+tort or otherwise, arising from, out of or in connection with the software
+or the use or other dealings in the software.
+
+Inquiries: www.calendriermilesien.org
 */
 function milesianAlertMsg (inputError) {	// Prepare an error message in suitable language
 	var	languages = [ "en", "fr", "de", "es", "pt" ];
