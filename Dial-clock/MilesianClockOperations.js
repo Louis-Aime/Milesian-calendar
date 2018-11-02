bc359244f63afaa1b4bb3977a31b0a7b5bc3d9a8
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2018-11-02 14:35:25 +0100

    Name change + JSDoc
    Routine name changed, with backward compatibility

--- Dial-clock/MilesianClockOperations.js
+++ Dial-clock/MilesianClockOperations.js
@@ -1,4 +1,4 @@
-/* Milesian Solar Year Clock Handler
+/* Milesian Calendar Clock Handler
 	Character set is UTF-8
 	Operations on Milesian solar year clock - set clock hands to right angle
 		may handle month, day, hour, minute and second hands. All hands do not need to exist.
@@ -6,6 +6,10 @@ Versions
 	M2017-11-07: add an "am/pm" indicator
 	M2017-12-26: add year indication, and change parameters list
 	M2018-10-26: enhance comments
+	M2018-11-11: Change name, JSDocs comments
+Contents
+	setMilesianCalendarClockHands (new name for setSolarYearClockHands)
+	setSolarYearClockHands (deprecated)
 */
 /* Copyright Miletus 2017-2018 - Louis A. de Fouquières
 Permission is hereby granted, free of charge, to any person obtaining
@@ -28,10 +32,34 @@ tort or otherwise, arising from, out of or in connection with the software
 or the use or other dealings in the software.
 Inquiries: www.calendriermilesien.org
 */
+/** Stub in order to change name
+ * @deprecated setSolarYearClockHands
+*/
 function setSolarYearClockHands(clock, year = undefined, month = 0, day = 1, hour = 24, minute = 0, second = 0, continuous = false) { 
-//	On "clock" object, set all available hands (month, day, hour, minutes, seconds)
-//  Unless "continuous" is specified as true, set month and day hands at end of day.
-//  If no date given, set to 1 1m.
+	return setMilesianCalendarClockHands(clock, year, month, day, hour, minute, second, continuous)
+}
+/** Set the hands of a milesian (solar) clock, after the components of a milesian date.
+ * @since M2018-11-11
+ * @param {Object} clock - a graphical object that represents the clock, that the routine will set
+ * if existing, these elements shall be set
+ *   @member .yearDisplay where the year is displayed
+ *   @member .clockhand.month a hand to be rotated, indicates the month
+ *   @member .clockhand.day a hand to be rotated, indicates the day in month
+ *   @member .clockhand.hour a hand to be rotated, indicates the hour (12 hours dial)
+ *   @member .clockhand.minute a hand to be rotated, minutes
+ *   @member .clockhand.seconds a hand to be rotated, seconds
+ *   @member .ampm where "am" or "pm" shall be indicated
+ * @param {number} year - year, displayed as is in .yearDisplay
+ * @param {number} month - month, 0 (1m) by default
+ * @param {number} day - date in month, 1 by default
+ * @param {number} hour - hour, 0 to 24, 24 by default, which means end of day
+ * @param {number} minute - minute, 0 to 59, 0 by default
+ * @param {number} second - second, 0 to 59, 0 by default
+ * @param {boolean} continous - is set, day and month hands shall move slightly during the day, 
+ * if unset (default), hands move by one day quantum.
+ * @return {number} number of half-days since beginning of year.
+*/
+function setMilesianCalendarClockHands(clock, year = undefined, month = 0, day = 1, hour = 24, minute = 0, second = 0, continuous = false) { 
 	var timeUnits = ["month", "day", "hour", "minute", "second"] ;	// the time units enumerated.
 	let 	halfDays = 60*month + 2*Math.floor(month/2) 
 			+ (continuous ? 2*(day-1) + (hour + minute/60 + second/3600)/12 : 2*day);	
