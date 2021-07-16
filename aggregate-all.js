4fa7c824d15b53bd5f72f1be691e7d6c174c2309
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2021-07-16 23:46:34 +0200

    Adapt to calendrical-javascript

--- aggregate-all.js
+++ aggregate-all.js
@@ -1,7 +1,9 @@
 /* aggregation module for Milesian-calendar: all entries collected and reexported from here */
 export * from "https://louis-aime.github.io/calendrical-javascript/chronos.js";
-export * from "https://louis-aime.github.io/calendrical-javascript/dateextended.js";
+export { default as ExtDate } from "https://louis-aime.github.io/calendrical-javascript/extdate.js";
+export { default as ExtDateTimeFormat } from "https://louis-aime.github.io/calendrical-javascript/extdatetimeformat.js";
 export * from "https://louis-aime.github.io/calendrical-javascript/calendars.js";
+export { default as fetchDOM } from "https://louis-aime.github.io/calendrical-javascript/fetchdom.js";
 export * from "./seasons.js";
 export * from "./calendarclock.js";
 export * from "./countconversion.js";
