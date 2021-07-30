4ad57ce84ad72bc213736be040f35f2efe48a383
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2021-07-30 21:56:48 +0200

    Use promises
    This initiator use promises. It is the same for all pages, for the web sites. Second part (event listeners) are specific by page.

--- milesianclockinitiate.js
+++ milesianclockinitiate.js
@@ -5,26 +5,9 @@ They use the basic Milesian calendar functions, and the conversion functions of 
 in order to display the Milesian on-line clock and to perform calendar conversion.
 Associated with: 
 *	MilesianClock.html
-1. Common functions
-*	CBCCE (the Cycle-Based Calendar Computation Engine)
-*	MilesianAlertMsg
-*	getRealTZmsOffset method
-2. For conversions and clock operation
-*	MilesianClockOperations
-*	LunarClockOperation
-*	MilesianDateProperties
-*	JulianDateProperties
-*	FrenchRevDateProperties
-*	IsoWeekCalendarDateProperties
-*	LunarDateProperties
-*	ChronologicalCountConversion
-3. For display, using Unicode standards
-*	UnicodeBasic
-*	UnicodeMilesianFormat (used to be: toMilesianLocaleDateString then UnicodeMilesian)
-*	MilesianMonthNameString (indirectly - or access to the name base in XML)
 */
-/* Version notes
-	This .js file is highly related to the corresponding html code. 
+/* Version notes (follow GitHub)
+	This file aims ar created an adequate environment for several milesian clock applications.. 
 	This part is especially for initialisation.
 */
 /* Version	M2021-08-07
@@ -55,158 +38,24 @@ Inquiries: www.calendriermilesien.org
 */
 "use strict";
 var	// global variables at document level.
-	modules,		// all modules here once imported
-	milesian,	// the Milesian calendar (plays a special role)
-	calendars = [],	// an array (pointers to) calendar objects
-	targetDate = undefined, 	// new Date(),	// Reference UTC date
-	customCalIndex = 0,	// initialised and later changed.
-	switchingDate = { year : 1582, month : 12, day : 20 },
-	TZ = "", 	// time zone for specifying a date; "" means "system", only alternative value is "UTC".
-	TZOffset = 0,
-	// TZSettings : {mode : "", msoffset : 0},	// deprecate
-	Options = {weekday : "long", day : "numeric", month: "long", year : "numeric", 
-			hour : "numeric", minute : "numeric", second : "numeric"}, 	
-	askedOptions,	// for custom calendar
-	userOptions,	// 
-	unicodeFormat,	// for Unicode selected calendar
-	unicodeOptions,
-	clockFormat,
-	astroCalend,
-	weekFormat,		// formater for day of week for current custom calendars
-	DOWFormat,		// formater for the traditional week day, used for the yearly figures
-	romanDateFormat,	// monthDay for julian and gregorian calendar
-	lunarDateFormat, // monthDay to indicate a general astronomical event 
-	dracoDateFormat,	// year month day
-	seasonDateFormat,	// year month day hours minutes
-	milesianClock;
+	// all modules here once imported
+	// pldrDOM,		// imported PLDR
+	modules,	// imported modules
+	pldrDOM;	// pldrDOM,
 
-(async function () {	// Fetching modules and XML data, initialising
-	modules = await import ('./aggregate-all.js');
-	let pldrString = await import ('https://louis-aime.github.io/calendrical-javascript/pldr.js');
-	let	pldrDOM = await modules.fetchDOM ("https://louis-aime.github.io/calendrical-javascript/pldr.xml")
-			.then ( (pldrDOM) => pldrDOM ) // The pldr data used by the Milesian calendar (and possibly others).
-			.catch ( (error) => { return pldrString.default() } );	// if error (no XML file) take default pldr 
-	milesian = new modules.MilesianCalendar ("milesian",pldrDOM);
-	calendars.push (milesian);
-	calendars.push (new modules.GregorianCalendar ("gregorian"));
-	calendars.push (new modules.JulianCalendar ("julian"));
-	calendars.push (new modules.WesternCalendar ("historic", modules.ExtDate.fullUTC(switchingDate.year, switchingDate.month, switchingDate.day)));
-	calendars.push (new modules.FrenchRevCalendar ("frenchrev"));
-	targetDate = new modules.ExtDate(milesian);
-	milesianClock = new modules.SolarClock( document.querySelector("#mclock") );
-	customCalIndex = calendars.findIndex (item => item.id == document.custom.calend.value);  // change custom calendar
-	compLocalePresentationCalendar();
-	clockAnimate.clockRun(1);	// start clock
-})();
 
-window.onload = function () {	// Initiate fields and set event listeners
+const // Promises of loading initial files.
+	modulesload = import ("./aggregate-all.js")
+		.then ( (modulesload) =>  { modules = modulesload } ),
+	pldrload  = import ("./calendrical-javascript/fetchdom.js").then	// should add a fallback with pldr.js
+		( (value) => value.default ('https://louis-aime.github.io/calendrical-javascript/pldr.xml', 1000) ) .then (
+			(value) => { pldrDOM = value },			// fetching XML file has succeeded.
+			(error) => {							// fetching XML has failed, we use the fallback value
+				console.log ('pldrDOM not fetched from its repository, fetching pldr.js');
+				return import ("./calendrical-javascript/pldr.js").then 
+					( (value) => pldrDOM = value.default () ) 
+					}
+			),
+	loadComplete = Promise.all([modulesload, pldrload]);
 
-	document.gregorianswitch.day.value = switchingDate.day;
-	document.gregorianswitch.month.value = switchingDate.month;
-	document.gregorianswitch.year.value = switchingDate.year;
 
-	document.gregorianswitch.addEventListener("submit", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		let 
-			day =  Math.round (event.srcElement.elements.day.value),
-			month = event.srcElement.elements.month.value,
-			year =  Math.round (event.srcElement.elements.year.value),
-			testDate = new modules.ExtDate (calendars.find(item => item.id == "gregorian"),modules.ExtDate.fullUTC(year, month, day)),
-			index = calendars.findIndex (item => item.id == "historic");
-		if ( (testDate.valueOf() >= Date.UTC(1582,9,15,0,0,0,0)) && (testDate.day() == day) ) 
-			calendars[index] = new modules.WesternCalendar("historic", testDate.valueOf())
-		else alert ("Invalid switching date to Gregorian calendar: " + day + '/' + month + '/' + year );
-		// confirm current switching date
-		[document.gregorianswitch.day.value, document.gregorianswitch.month.value, document.gregorianswitch.year.value ]
-			= [ switchingDate.day, switchingDate.month, switchingDate.year ] = [ day, month, year ];
-		compLocalePresentationCalendar();	// because we changed one calendar, disseminate change.
-		if (calendars[customCalIndex].id == "historic") targetDate = new modules.ExtDate (calendars[index], targetDate.valueOf());	
-			// sweep former historic calendar out of current data
-		setDisplay();
-	})	
-		
-	document.custom.addEventListener("submit", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		clockAnimate.clockRun(0);
-		calcCustomDate()
-	})
-	document.custom.calend.addEventListener("blur", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		customCalIndex = calendars.findIndex (item => item.id == document.custom.calend.value);  // change custom calendar
-		targetDate = new modules.ExtDate(calendars[customCalIndex], targetDate.valueOf());	// set custom calendar if changed, and set date.
-		compLocalePresentationCalendar();
-		setDisplay();
-	})
-
-	document.week.addEventListener("submit", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		clockAnimate.clockRun(0);
-		calcWeek()
-	})
-	document.daycounter.addEventListener("submit", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		clockAnimate.clockRun(0);
-		calcJulianDay()
-	})
-
-	document.control.addEventListener("submit", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		clockAnimate.changeDayOffset()
-	})
-	document.control.now.addEventListener("click", function (event) {
-		clockAnimate.clockRun(0);
-		setDateToNow()
-	})
-	document.control.minus.addEventListener("click", function (event) {
-		clockAnimate.clockRun(0);
-		clockAnimate.setDayOffset(-1)
-	})
-	document.control.plus.addEventListener("click", function (event) {
-		clockAnimate.clockRun(0);
-		clockAnimate.setDayOffset(+1)
-	})
-
-	document.time.addEventListener("submit", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		clockAnimate.clockRun(0);
-		calcTime()
-	})
-
-	document.timeShift.addEventListener("submit", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		myTimeShift.changeAddTime()
-	})
-	document.timeShift.minus.addEventListener("click", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		clockAnimate.clockRun(0);
-		myTimeShift.changeAddTime()
-		myTimeShift.addTime(-1)
-	})
-	document.timeShift.plus.addEventListener("click", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		clockAnimate.clockRun(0);
-		myTimeShift.changeAddTime()
-		myTimeShift.addTime(+1)
-	}) 
-
-	document.TZmode.addEventListener("submit", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		TZ = document.TZmode.TZcontrol.value;
-		setDisplay();
-	})
-
-	document.getElementById("h0").addEventListener("click", function (event) {
-		clockAnimate.clockRun(0);
-		setUTCHoursFixed(0)
-	})
-	document.getElementById("h12").addEventListener("click", function (event) {
-		clockAnimate.clockRun(0);
-		setUTCHoursFixed(12)
-	})
-
-	document.LocaleOptions.addEventListener ("submit", function (event) {
-		event.preventDefault();		// necessary to avoid re-loading with multi-fields forms especially when fields are fetched from event.
-		compLocalePresentationCalendar();
-		setDisplay()
-	})
-}
